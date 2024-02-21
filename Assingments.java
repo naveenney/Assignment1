faf8eaf8b5aed20ea5491a4b5ef16e16f448ef55
naveenney <159130020+naveenney@users.noreply.github.com>
2024-02-21 13:33:06 +0530

    Add files via upload

---
+++ Assingments.java
@@ -0,0 +1,25 @@
+
+public class Assingments {
+    
+	class Vehicle 
+	{
+		
+	}
+	
+	class Car extends Vehicle
+	{
+		
+	}
+	
+	class Bike extends Vehicle
+	{
+	
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("4 class f")
+	}
+	
+}
